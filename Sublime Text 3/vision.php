cbf7c6e3be48f51c46a606552aaaf1a7f74cc828
RomualdDev <romuald91303142@gmail.com>
2023-04-07 18:14:11 +0000

    mon commit

---
+++ Sublime Text 3/vision.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>reprise</title>
+</head>
+<body>
+
+	<p>le paragraphe</p>
+
+	<?php  echo "ceci est du php "  ?>
+
+</body>
+</html>
---
+++ acceuil.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
+    <link rel="stylesheet" href="acceuil.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <head>
+        <nav class="navigation">
+            <div class="nav d-flex">
+                <div class="mon_logo">
+                    <a href="#" class="logo"> <span>H</span>ope</a>
+                </div>
+                <div class="icone">
+                    <i class='bx bx-menu'></i>
+                </div>
+                <div class="menu d-flex"> 
+                <div class="top">
+                    <span class="croix"><i class='bx bxs-right-arrow-circle'></i></span>
+                </div>
+                    <ul class="menu-ul d-flex">
+                        <li class="menu-li"><a href="#" class="menu-link">ACCUEIL</a></li>
+                        <li class="menu-li"><a href="#" class="menu-link">VERIFICATION DE TICKET</a></li>
+                        <li class="menu-li"><a href="#" class="menu-link">CONTACT</a></li>
+                    </ul>
+                    
+                </div>
+            </div>
+
+
+
+        </nav>
+
+
+
+
+
+
+    </head>
+    
+</body>
+</html>
